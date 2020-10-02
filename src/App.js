b44b6d9ce66f8ae8af83675c4e218c0f269196ad
kabir-webDev <kabir.webdev@gmail.com>
2020-10-02 21:29:32 +0600

    Deployed

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import LineCharts from "./components/LineCharts";
 function App() {
   return (
     <div className="App container">
+      <h1 style={{ color: " rgba(238, 86, 59, 0.856)" }}>
+        কাবির সাহেবের রেজাল্ট কার্ভ
+      </h1>
       <div className="neu">
         <LineCharts />
       </div>
+
+      <h1 style={{ color: " rgba(238, 86, 59, 0.856)" }}>
+        এই রেজাল্টে কি জব হবে!! 🥺
+      </h1>
     </div>
   );
 }
