6011c710c8896a13e91c512057fea76acb8052e1
kabir-webDev <kabir.webdev@gmail.com>
2020-10-02 21:13:41 +0600

    Updates

---
+++ precache-manifest.6c80b87f41aeb55d108b9fedc4c7f06b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30f6fc9769e689cae8cc5116cdf0b55",
+    "url": "/chartjs/index.html"
+  },
+  {
+    "revision": "bfa0383ce4714c4b2624",
+    "url": "/chartjs/static/css/main.6f202967.chunk.css"
+  },
+  {
+    "revision": "cc67b5749a833015005e",
+    "url": "/chartjs/static/js/2.52b221ff.chunk.js"
+  },
+  {
+    "revision": "d2648fb43d975500430a657c7ecabae1",
+    "url": "/chartjs/static/js/2.52b221ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa0383ce4714c4b2624",
+    "url": "/chartjs/static/js/main.2f8f62d1.chunk.js"
+  },
+  {
+    "revision": "1f6b0c3d8874c1b3a8fd",
+    "url": "/chartjs/static/js/runtime-main.e97f27aa.js"
+  }
+]);
